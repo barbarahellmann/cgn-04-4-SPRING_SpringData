dac1fccd6c2865153ef9d4ba900e0db4d515e863
barbarahellmann <babshellmann@web.de>
2024-04-03 15:10:31 +0200

    Playing around with MongoDB

--- src/main/java/de/neuefische/cgnjava241mongodb/Student.java
+++ src/main/java/de/neuefische/cgnjava241mongodb/Student.java
@@ -1,4 +1,8 @@
 package de.neuefische.cgnjava241mongodb;
 
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Students")
 public record Student(String id, String name, int age) {
 }
