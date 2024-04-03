627639ee6862ecd450b328eda84f5c47a23f6db8
barbarahellmann <babshellmann@web.de>
2024-04-03 14:21:20 +0200

    TextController adjusted to MongoDB

--- src/main/java/de/neuefische/cgnjava241mongodb/controller/TestController.java
+++ src/main/java/de/neuefische/cgnjava241mongodb/controller/TestController.java
@@ -2,34 +2,14 @@ package de.neuefische.cgnjava241mongodb.controller;
 
 import de.neuefische.cgnjava241mongodb.Student;
 import de.neuefische.cgnjava241mongodb.repository.TestRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-//wlVOUFIfBRQBqhpP
-@RestController
-@RequestMapping("/api/students")
-public class TestController {
+@Repository
+public interface TestController extends MongoRepository<Student, String> {
 
-    TestRepository repo = new TestRepository();
 
-    @GetMapping
-    public List<Student> getAllStudents(){
-        return repo.getAllStudents();
-    }
-
-    @GetMapping("/{id}")
-    public Student getStudentById(@PathVariable String id){
-        return repo.getStudentById(id);
-    }
-
-    @PostMapping
-    public void saveNewStudent(@RequestBody Student student){
-        repo.saveStudent(student);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteStudentById(@PathVariable String id){
-        repo.deleteStudent(id);
-    }
 }
